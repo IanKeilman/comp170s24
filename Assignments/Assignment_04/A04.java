a2f7eba16193a0756db7b78909ae02acbafeaccf
IanKeilman <160919382+IanKeilman@users.noreply.github.com>
2024-06-26 21:00:37 +0000

    Assignment_04

--- Assignments/Assignment_04/A04.java
+++ Assignments/Assignment_04/A04.java
@@ -1,8 +1,28 @@
 public class A04 {
+
     public static void main(String[] args) {
+
+        System.out.print("Odd characters: ");
         printOddCharacters("Springfield"); // Expect: p i g i l
-        printEvenCharacters("Springfield"); // Expect: S r n f e d 
-    } // method main
 
+        System.out.print("Even characters: ");
+        printEvenCharacters("Springfield"); // Expect: S r n f e d
+    }
 
+    // Method to print characters found in odd positions
+    static void printOddCharacters(String string) {
+        for (int position = 1; position < string.length(); position += 2) {
+            System.out.print(string.charAt(position) + " ");
+        }
+        System.out.println();
+    }
+
+    // Method to print characters found in even positions
+    static void printEvenCharacters(String string) {
+        for (int position = 0; position < string.length(); position += 2) {
+            System.out.print(string.charAt(position) + " ");
+        }
+        System.out.println();
+        
+    }
 }
