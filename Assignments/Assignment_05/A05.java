eb5e6870686b9074c3eec57dac0b711924bb9fac
IanKeilman <160919382+IanKeilman@users.noreply.github.com>
2024-07-03 20:50:46 +0000

    Done.

--- Assignments/Assignment_05/A05.java
+++ Assignments/Assignment_05/A05.java
@@ -5,5 +5,20 @@ public class A05 {
         System.out.println(isPalindrome("Java")); // expect false
     } // method main
 
+    static boolean isPalindrome(String string) {
+        int left = 0;  // Starting with the first spot
+        int right = string.length() - 1; // starting with the last spot
+
+        while (left < right) { // only checks for while the left in is to the left of the right int
+            if (string.charAt(left) != string.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--; //left goes right, while right goes left
+        }
+
+        return true;
+    }
+
 
 }
